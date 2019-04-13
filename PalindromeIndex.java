4bf1cc9407dfc2be28599fa220bf29c488ca5545
Ng Iris <43512172+Iris2000@users.noreply.github.com>
2019-04-13 20:55:08 +0800

    Add files via upload

---
+++ PalindromeIndex.java
@@ -0,0 +1,39 @@
+// this program can detects whether a string is a palindrome
+import java.util.Scanner;
+
+public class PalindromeIndex {
+
+    public static void main(String[] args) {
+
+        String original;
+        char con;
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter a string to check whether it is a palindrome: ");
+            original = input.nextLine().toLowerCase();
+
+            int begin;
+            int length = original.length();
+            int middle = length / 2;
+
+
+            for (begin = 0; begin <= middle; begin++) {
+
+                if (original.charAt(begin) != original.charAt(length - begin - 1)) {
+                    System.out.println("false");
+                    break;
+                }
+            }
+
+            if (begin == middle + 1)
+                System.out.println("true");
+
+            System.out.print("Do you want to continue[y/n]: ");
+            con = input.next().toLowerCase().charAt(0);
+            input.nextLine();
+
+        } while (con == 'y');
+    }
+}
