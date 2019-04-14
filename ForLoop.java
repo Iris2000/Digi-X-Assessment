b8b004c2a32634a07f1dcfc75ccb284b5fdc43b2
Ng Iris <43512172+Iris2000@users.noreply.github.com>
2019-04-14 19:15:40 +0800

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -2,19 +2,21 @@
 this program use a for loop to prints
 all the multiples of 3 and 5 up to 100
 */
-
+import java.util.ArrayList;
 public class ForLoop {
 
     public static void main(String[] args) {
 
-        String result = "";
+        ArrayList<String> array = new ArrayList<>();
 
         for (int num = 1; num <= 100; num++) {
 
-            if (num % 3 == 0 || num % 5 == 0)
-                result = result + num + ", ";
+            if (num % 3 == 0 || num % 5 == 0) {
+                array.add(String.valueOf(num));
+            }
         }
 
-        System.out.print(result.replaceAll(", $", ""));
+        System.out.print(String.join(", ", array));
+
     }
 }
